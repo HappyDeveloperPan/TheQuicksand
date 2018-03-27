e4e1444af60533cb09c2caf599e410f9f15eaf44
pan <943147350@qq.com>
2018-03-27 09:20:55 +0800

    添加个人中心

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -11,5 +11,24 @@ Page({
         return util.formatTime(new Date(log))
       })
     })
+  },
+  kindToggle: function (e) {
+    var id = e.currentTarget.id, list = this.data.list;
+    for (var i = 0, len = list.length; i < len; ++i) {
+      if (list[i].id == id) {
+        if (list[i].url) {
+          wx.navigateTo({
+            url: 'pages/' + list[i].url
+          })
+          return
+        }
+        list[i].open = !list[i].open
+      } else {
+        list[i].open = false
+      }
+    }
+    this.setData({
+      list: list
+    });
   }
 })
